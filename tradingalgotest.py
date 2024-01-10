b6e6ae138919037cd1f1baf61f9c54253be9652b
Francisco Boudagh <franciscoboudagh1@gmail.com>
2024-01-10 19:59:31 +0100

    Added new technical indicator: Stochastic

--- tradingalgotest.py
+++ tradingalgotest.py
@@ -21,6 +21,14 @@ def calculate_macd(data, short_window=12, long_window=26, signal_window=9):
     signal_line = macd_line.ewm(span=signal_window, adjust=False).mean()
     return macd_line, signal_line
 
+# Stochastic Oscillator Calculation Function
+def calculate_stochastic_oscillator(data, k_window=14, d_window=3):
+    low_min = data['Low'].rolling(window=k_window).min()
+    high_max = data['High'].rolling(window=k_window).max()
+    K_line = ((data['Close'] - low_min) / (high_max - low_min)) * 100
+    D_line = K_line.rolling(window=d_window).mean()
+    return K_line, D_line
+
 
 """ # OPTION 1: Read CSV file
 df = pd.read_csv('AAPL.csv')
@@ -48,16 +56,19 @@ def plot_with_indicator(df, indicator, ticker_symbol):
         rsi = calculate_rsi(close_prices)
         buy_signals = rsi < 30
         sell_signals = rsi > 70
-        plt.scatter(df['Date'][buy_signals], close_prices[buy_signals], color='green', label='Buy Signal', marker='^')
-        plt.scatter(df['Date'][sell_signals], close_prices[sell_signals], color='red', label='Sell Signal', marker='v')
 
     elif indicator == 'MACD':
         macd_line, signal_line = calculate_macd(close_prices)
         buy_signals = (macd_line > signal_line) & (macd_line.shift() <= signal_line.shift())
         sell_signals = (macd_line < signal_line) & (macd_line.shift() >= signal_line.shift())
-        plt.scatter(df['Date'][buy_signals], close_prices[buy_signals], color='green', label='Buy Signal', marker='^')
-        plt.scatter(df['Date'][sell_signals], close_prices[sell_signals], color='red', label='Sell Signal', marker='v')
 
+    elif indicator == 'Stochastic':
+        K_line, D_line = calculate_stochastic_oscillator(df)
+        buy_signals = (K_line > D_line) & (K_line < 20)
+        sell_signals = (K_line < D_line) & (K_line > 80)
+
+    plt.scatter(df['Date'][buy_signals], close_prices[buy_signals], color='green', label='Buy Signal', marker='^')
+    plt.scatter(df['Date'][sell_signals], close_prices[sell_signals], color='red', label='Sell Signal', marker='v')
     plt.title(f'{ticker_symbol} with {indicator} Indicator. Buy/Sell Signals{title_suffix}')
     plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
     plt.gca().xaxis.set_major_locator(mdates.MonthLocator())
@@ -70,6 +81,6 @@ def plot_with_indicator(df, indicator, ticker_symbol):
 
 
 # Choose indicator and plot
-# Options: 'RSI', 'MACD'
-indicator_choice = 'RSI'
+# Options: 'RSI', 'MACD', 'Stochastic'
+indicator_choice = 'MACD'
 plot_with_indicator(df, indicator=indicator_choice, ticker_symbol=ticker_symbol)
